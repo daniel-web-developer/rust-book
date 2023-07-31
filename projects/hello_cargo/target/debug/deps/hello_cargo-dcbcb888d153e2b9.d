55e6ba21ad14e3df47de509d537e91fe86e66bfc
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-08-01 00:08:46 +0200

    chapter 2 done

---
+++ projects/hello_cargo/target/debug/deps/hello_cargo-dcbcb888d153e2b9.d
@@ -0,0 +1,5 @@
+/home/coder/Desktop/coding/rust/rust-book/projects/hello_cargo/target/debug/deps/hello_cargo-dcbcb888d153e2b9: src/main.rs
+
+/home/coder/Desktop/coding/rust/rust-book/projects/hello_cargo/target/debug/deps/hello_cargo-dcbcb888d153e2b9.d: src/main.rs
+
+src/main.rs:
---
+++ projects/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/coder/Desktop/coding/rust/rust-book/projects/hello_cargo/target/debug/hello_cargo: /home/coder/Desktop/coding/rust/rust-book/projects/hello_cargo/src/main.rs
