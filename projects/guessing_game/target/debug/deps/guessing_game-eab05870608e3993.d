424a1da449b4b1705860dc2b7e02baf134781b3e
daniel-web-developer <daniel_caixeta@protonmail.com>
2023-08-01 17:06:11 +0200

    first part guessing game

---
+++ projects/guessing_game/target/debug/deps/guessing_game-eab05870608e3993.d
@@ -0,0 +1,5 @@
+/home/coder/Desktop/coding/rust/rust-book/projects/guessing_game/target/debug/deps/guessing_game-eab05870608e3993: src/main.rs
+
+/home/coder/Desktop/coding/rust/rust-book/projects/guessing_game/target/debug/deps/guessing_game-eab05870608e3993.d: src/main.rs
+
+src/main.rs:
---
+++ projects/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/coder/Desktop/coding/rust/rust-book/projects/guessing_game/target/debug/guessing_game: /home/coder/Desktop/coding/rust/rust-book/projects/guessing_game/src/main.rs
